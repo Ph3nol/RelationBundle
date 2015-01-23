0d8ba8cf9da0cfbfc60b48ba1257401b0f87e837
Joy Lazari <joy.lazari@kopjra.com>
2015-01-23 14:58:24 +0100

    + testRelationExistMustReturnBool

--- Tests/Manager/ManagerTest.php
+++ Tests/Manager/ManagerTest.php
@@ -1,19 +1,32 @@
 <?php
 
-use Sly\RelationBundle\Manager\Manager;
 
 /**
  * Manager tests.
  *
  * @author Cédric Dugat <ph3@slynett.com>
  */
-class ManagerTest extends \PHPUnit_Framework_TestCase
-{
+class ManagerTest extends PHPUnit_Framework_TestCase {
+
     /**
-     * Test Manager.
+     * @var AppKernel
      */
-    public function testManager()
-    {
-        $this->markTestIncomplete('Todo: write some tests!');
+    public $kernel;
+
+    public function setUp(){
+        $this->kernel = new AppKernel('dev', true);
+        $this->kernel->boot();
+    }
+
+    public function testRelationExistMustReturnBool() {
+
+        $relation = $this->kernel->getContainer()->get('sly_relation');
+        $relation->relationShip(
+            'friendship',
+            $this->kernel->getContainer()->get('doctrine')->getRepository('AcmeDemoBundle:User')->find(3),
+            $this->kernel->getContainer()->get('doctrine')->getRepository('AcmeDemoBundle:User')->find(3)
+        );
+
+        $this->assertTrue(is_bool($relation->exists()));
     }
 }
